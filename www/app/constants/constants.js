0713a8d98bf3e77bfa2a74afeab5c1da986ef562
prosperusxix <tobal.vlp@gmail.com>
2015-05-07 19:02:07 -0300

    foto fondo post

--- www/app/constants/constants.js
+++ www/app/constants/constants.js
@@ -2,34 +2,36 @@
 //var url_base = 'http://lufke-server-44936.onmodulus.net'; //server modulus
 //var url_base = 'http://10.0.2.2:3000'; //server local dentro del emulador
 //var url_base = 'http://192.168.0.100:3000'; //server local dentro del emulador
-var url_base = 'http://localhost:53110/api';
+var url_base = 'http://localhost:8080/api'; 
 //var url_base = 'http://192.168.0.100:53110/api';
-//var url_base = 'http://10.0.2.2:53110/api';
+//var url_base = 'http://10.0.2.2:8080/api';
+//var url_base = 'http://192.168.0.101:8080/api';
+var url_files = 'http://localhost:8080';
 var api = {
-    user: {
-        login: url_base + '/user/login',
-        register: url_base + '/user/register'
-    },
-    post: {
-        get: url_base + '/post/get',
-        getAll: url_base + '/news/getnews',
-        create: url_base + '/post/create',
-        delete: url_base + '/post/delete',
-        toggleLike: url_base + '/post/togglelikepost',
-        uploadTest: url_base + '/post/uploadImage',
-        comment: {
-            create: url_base + '/post/addcomment',
-            delete: url_base + '/post/deletecomment'
+        user: {
+            login: url_base + '/user/login',
+            register: url_base + '/user/register'
+        },
+        post: {
+            get: url_base + '/post/get',
+            getAll: url_base + '/news/getnews',
+            create: url_base + '/post/create',
+            delete: url_base + '/post/delete',
+            toggleLike: url_base + '/post/togglelikepost',
+            uploadTest: url_base + '/post/uploadImage',
+            comment: {
+                create: url_base + '/post/addcomment',
+                delete: url_base + '/post/deletecomment'
+            }
+        },
+        explore: {
+            getPopulars: url_base + '/explore/getexploredata',
+            followCategory: url_base + '/explore/followcategory',
+            followUser: url_base + '/notification/followuser'
+        },
+        notifications: {
+            getNotifications: url_base + '/notification/getnotificationsdata'
         }
-    },
-    explore: {
-        getPopulars: url_base + '/explore/getexploredata',
-        followCategory: url_base + '/explore/followcategory',
-        followUser: url_base + '/notification/followuser'
-    },
-    notifications: {
-        getNotifications: url_base + '/notification/getnotificationsdata'
-    }
 };
 
 function getPostBackgroundUlr(post) {
