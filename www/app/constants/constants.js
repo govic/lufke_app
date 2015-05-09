38fc16af4c381aefd207b225dea0d132cffc065a
Hector <hectorfuentesg@gmail.com>
2015-05-09 12:43:32 -0400

    get perfil

--- www/app/constants/constants.js
+++ www/app/constants/constants.js
@@ -2,25 +2,18 @@
 //var url_base = 'http://lufke-server-44936.onmodulus.net'; //server modulus
 //var url_base = 'http://10.0.2.2:3000'; //server local dentro del emulador
 //var url_base = 'http://192.168.0.100:3000'; //server local dentro del emulador
-<<<<<<< HEAD
-var url_base = 'http://localhost:8080/api';
-//var url_base = 'http://192.168.0.100:53110/api';
-//var url_base = 'http://10.0.2.2:8080/api';
-//var url_base = 'http://192.168.0.101:8080/api';
-var url_files = 'http://localhost:8080';
-=======
 //var url_base = 'http://192.168.0.100:53110/api';
 //var url_base = 'http://10.0.2.2:8080/api';
 //var url_base = 'http://localhost:8080/api'; 
 //var url_files = 'http://localhost:8080';
-var url_base = 'http://192.168.0.101:8080/api'; //sirve para local y celular
-var url_files = 'http://192.168.0.101:8080'; //sirve para local y celular
+var url_base = 'http://localhost:8080/api'; //sirve para local y celular
+var url_files = 'http://localhost:8080'; //sirve para local y celular
 
->>>>>>> e1a30b52239abe622b23590982b46c7b6c6bd863
 var api = {
     user: {
         login: url_base + '/user/login',
-        register: url_base + '/user/register'
+        register: url_base + '/user/register',
+        getProfile: url_base + '/user/getprofile'
     },
     post: {
         get: url_base + '/post/get',
--- www/app/controllers/profile.controller.js
+++ www/app/controllers/profile.controller.js
@@ -1,61 +1,37 @@
-angular.module('lufke').controller('ProfileController', function ($scope, PostsService, $state) {
-	console.log('Inicia ... ProfileController');
-
-	$scope.model = {
-		backgroundImgUrl: "http://goo.gl/WgH5Ju",
-		profileImgUrl: "http://goo.gl/T4xQbr",
-		profileFirstName: "Ryan",
-		profileLastName: "Sheckler",
-		profileId: 1,
-		postsNumber: 2093,
-		postsUnit: "",
-		followersNumber: 5.4,
-		followersUnit: "M",
-		followingNumber: 204,
-		followingUnit: "",
-		interests: [
-			{
-				interestText: "Surfing",
-				interestId: 1
-			}, {
-				interestText: "Hiking",
-				interestId: 2
-			}, {
-				interestText: "Skateboard",
-				interestId: 3
-			}
-		],
-		summaryText: "Skateboard fan and I'm from San Clemente",
-		tags: [
-			{
-				tagText :"@Red-Bull",
-				tagId: 1
-			}, {
-				tagText: "Plan B",
-				tagId: 2
-			}, {
-				tagText: "@Etnies",
-				tagId: 3
-			}
-		],
-		socialLinkText: "www.socialink.me/ryansheckler",
-		socialLinkUrl: "http://www.socialink.me/ryansheckler?origin=lufkeapp",
-		lastPosts: []
-	};
-	
-	$scope.$on('$ionicView.afterEnter', function () {
-		//TODO: el servicio debe encargarse de actualizar cambios en la lista de noticias
-		console.log("Entro a... ProfileController");
-		$scope.model.lastPosts = PostsService.getLastUserPosts($scope.model.profileId, 0);
-	});
-
-	$scope.editProfile = function (id) {
-		$state.go('editprofile', {'id': id});
-	}
-	$scope.viewTag = function (tag) {
-		alert("Ver detalle tag id = " + tag.tagId);
-	}
-	$scope.socialLink = function (link) {
-		window.open(link, '_system', 'location=yes');
-	};
+angular.module('lufke').controller('ProfileController', function($scope, PostsService, $state, $http) {
+    console.log('Inicia ... ProfileController');
+    $http.post(api.user.getProfile).success(function(profile, status, headers, config) {
+        $scope.model = profile;
+    }).error(function(data, status, headers, config) {
+        console.dir(data);
+        console.log(status);
+        $scope.showMessage("Error", "Ha ocurrido un error al cargar sus datos de perfil.");
+    });
+    $scope.$on('$ionicView.afterEnter', function() {
+        //TODO: el servicio debe encargarse de actualizar cambios en la lista de noticias
+        console.log("Entro a... ProfileController");
+        //$scope.model.lastPosts = PostsService.getLastUserPosts($scope.model.profileId, 0);
+    });
+    $scope.editProfile = function(id) {
+        $state.go('editprofile', {
+            'id': id
+        });
+    }
+    $scope.viewTag = function(tag) {
+        alert("Ver detalle tag id = " + tag.tagId);
+    }
+    $scope.socialLink = function(link) {
+        window.open(link, '_system', 'location=yes');
+    };
+    $scope.showMessage = function(title, message, callback) {
+        var alertPopup = $ionicPopup.alert({
+            title: title,
+            template: message,
+            okText: "Aceptar"
+        });
+        alertPopup.then(function(res) {
+            if (callback) callback();
+            return;
+        });
+    };
 });
