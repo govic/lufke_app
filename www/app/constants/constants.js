62b95d3a22b2eb7edce4161fbf6e9e256135fe99
prosperusxix <tobal.vlp@gmail.com>
2015-05-19 16:38:41 -0300

    nada

--- www/app/constants/constants.js
+++ www/app/constants/constants.js
@@ -1,4 +1,4 @@
-var url_files = 'http://192.168.0.100:8080'; //sirve para local y celular
+var url_files = 'http://testlufkegobic.azurewebsites.net'; //sirve para local y celular
 //var url_files = 'http://localhost:8080'; //sirve para local y celular
 var url_base = url_files + '/api'; //sirve para local y celular
 
